9fdb37c1808d5ed91a2d8a220e02771b967694d7
Juarez Martins <juniorbmx13@gmail.com>
2023-02-23 21:52:48 -0300

    feat: add estudos básicos Javascript

---
+++ Javascript/basico/atribuicaoVariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 34;
+const nome = "Juarez";
+const sobrenome = "Martins";
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome}`);
