ac892c0e6a90ac12e764fedd2638ddeb813e99c3
Juarez Martins <juniorbmx13@gmail.com>
2023-02-24 19:35:28 -0300

    operadores lógicos com Javascript

--- Javascript/basico/listas.js
+++ Javascript/basico/listas.js
@@ -1,4 +1,4 @@
-console.log(`Trabalhando com listas`);
+console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(
     `Salvador`,
@@ -6,14 +6,28 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
+const idadeComprador = 15;
+const estaAcompanhado = true;
+
 listaDeDestinos.push(`Curutiba`);  //método add elementos
 
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
-listaDeDestinos.splice(1, 1);  // remover posição, qtdade itens
-
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
 console.log(listaDeDestinos[1]);  //chamar posição específica
+
+if(idadeComprador >= 18){
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1, 1);  // remover posição, qtdade itens
+}
+else{
+    if(estaAcompanhado){
+        console.log(`Comprador está acompanhado`);
+    }
+    else{
+        console.log("Não é maior de idade, não posso vender");
+    }
+}
