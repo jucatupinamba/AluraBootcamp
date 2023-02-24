4641c05a678ff9810a83d4d29c1a301edc0720cc
Juarez Martins <juniorbmx13@gmail.com>
2023-02-24 17:47:52 -0300

    conhecendo listas em Javascript

---
+++ Javascript/basico/listas.js
@@ -0,0 +1,12 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curutiba`);  //método add elementos
+
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
