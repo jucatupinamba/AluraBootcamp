b63c84490056dbb3f523e860b9a464415b45dcea
Juarez Martins <juniorbmx13@gmail.com>
2023-02-28 18:53:00 -0300

    feat: novo método add listas

--- Javascript/basico/1823_DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
+++ Javascript/basico/1823_DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
@@ -1,14 +1,23 @@
 const novaTarefa = document.querySelector('[data-form-button]');
 
+const criarTarefa = (evento) => {
 
-
-
-novaTarefa.addEventListener('click', (evento)=> {
+    evento.preventDefault();  
     
-    evento.preventDefault();    
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
-
     const valor = input.value;
     console.log(valor);
+    
+    const tarefa =  document.createElement('li');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    lista.appendChild(tarefa);
     input.value = " ";
-});
+
+}
+
+
+novaTarefa.addEventListener('click', criarTarefa);
