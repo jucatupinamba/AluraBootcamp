c38c6ce2dc6e75a1945070c833f7aa36bca1c689
Juarez Martins <juniorbmx13@gmail.com>
2023-02-28 15:50:03 -0300

    feat: manipulação de seletores e elementos

---
+++ Javascript/basico/1823_DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
@@ -0,0 +1,14 @@
+const novaTarefa = document.querySelector('[data-form-button]');
+
+
+
+
+novaTarefa.addEventListener('click', (evento)=> {
+    
+    evento.preventDefault();    
+    const input = document.querySelector('[data-form-input]');
+
+    const valor = input.value;
+    console.log(valor);
+    input.value = " ";
+});
