268014d1ac79def63dc51887c9dbe1603c15812b
Juarez Martins <juniorbmx13@gmail.com>
2023-03-03 18:49:56 -0300

    feat: functino in botton concluir .toggle

--- Javascript/basico/1823_DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
+++ Javascript/basico/1823_DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
@@ -1,4 +1,4 @@
-const novaTarefa = document.querySelector('[data-form-button]');
+
 
 const criarTarefa = (evento) => {
 
@@ -9,15 +9,37 @@ const criarTarefa = (evento) => {
     const valor = input.value;
     console.log(valor);
     
-    const tarefa =  document.createElement('li');
+    const tarefa =  document.createElement('li');  //criar elemento
+    tarefa.classList.add('task');  //add CSS
     const conteudo = `<p class="content">${valor}</p>`;
 
     tarefa.innerHTML = conteudo;
 
+    tarefa.appendChild(BotaoConclui());    
     lista.appendChild(tarefa);
     input.value = " ";
 
 }
 
+const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement(`button`);
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+    botaoConclui.addEventListener('click', concluirTarefa);
+
+        return botaoConclui;
+    
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle('done')
+}
