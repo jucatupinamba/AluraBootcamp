4c5f5b3d9b42e19973577557e765d778a802902b
Juarez Martins <juniorbmx13@gmail.com>
2023-03-09 09:17:18 -0300

    busca de erro

--- Javascript/basico/validacao-doguito-main/js/validacao.js
+++ Javascript/basico/validacao-doguito-main/js/validacao.js
@@ -1,26 +1,27 @@
-export function valida(input){
+export function valida(input) {
     const tipoDeInput = input.dataset.tipo
 
-    if(validadores[tipoDeInput]){
+    if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
 }
 
-const validadores =   {
+const validadores = {
     dataNascimento:input => validaDataNascimento(input)
 }
 
-function validaDataNascimento (inpupt) {
+function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
+    let mensagem = ''
 
-    if(!maiorQue18(dataRecebida)){
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar'
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 
     input.setCustomValidity(mensagem)
 }
 
-function maiorQue18 (data) {
+function maiorQue18(data) {
     const dataAtual = new Date()
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
