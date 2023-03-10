e76d5ae29e8e4967e43819dd988178dfd94baced
Juarez Martins <juniorbmx13@gmail.com>
2023-03-10 10:00:48 -0300

    atualização de projeto

--- Javascript/basico/validacao-doguito-main/js/validacao.js
+++ Javascript/basico/validacao-doguito-main/js/validacao.js
@@ -5,13 +5,12 @@ export function valida(input) {
         validadores[tipoDeInput](input)
     }
 
-    if(input.validity.valid){
-        input.parentElement.classList.add('input-container--invalido')
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
         input.parentElement.classList.add('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro
-        (tipoDeInput, input)
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
@@ -23,37 +22,35 @@ const tiposDeErro = [
 ]
 
 const mensagensDeErro = {
-    nome:{
-        valueMissing: 'O campo nome não pode estar vazio.'
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
     },
     email: {
         valueMissing: 'O campo de email não pode estar vazio.',
-        typeMismatch: 'O email digitado não é válido.' 
+        typeMismatch: 'O email digitado não é válido.'
     },
     senha: {
         valueMissing: 'O campo de senha não pode estar vazio.',
-        patternMismatch: "O primeiro caractere da senha deve ser uma letra, deve conter no mínimo"
-        + "4 caracteres e no máximo 15 caracteres e não podem ser usados ​​caracteres" +
-        "além de letras, números e o sublinhado.",
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
     },
     dataNascimento: {
-        valueMissing: 'O campo de nascimento não pode estar vazio',
-        customError: 'Você deve ser maior de 18 anos para se cadastrar'    
-    }   
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
 }
 
-function mostraMensagemDeErro(tipoDeInput, input){
+function mostraMensagemDeErro(tipoDeInput, input) {
     let mensagem = ''
     tiposDeErro.forEach(erro => {
-        if(input.validity[erro]){
+        if(input.validity[erro]) {
             mensagem = mensagensDeErro[tipoDeInput][erro]
         }
     })
-
+    
     return mensagem
 }
 
